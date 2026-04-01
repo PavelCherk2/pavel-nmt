0c2961f81277d1620e29ce740ddfd01c2f468d0b
Your Name01 <your.email@example.com>
2026-04-01 11:51:01 +0300

    2

--- src/components/Tests/Tests.js
+++ src/components/Tests/Tests.js
@@ -1,9 +1,15 @@
 import './Tests.css';
 
-const Tests = () => (
-  <div className="Tests">
-    Tests Component
+export function TestPage() {
+  return <div>
+    <div>
+      <button>Тестові питання</button>
+      <button>Питання з вводом</button>
+      <button>Питання на відповідность</button>
+    </div>
+    <div>
+      <OneAswerBlock topicId={1} subtopicId={1} questionType="four-options" />
+      <InputAnswerBlock topicId={1} subtopicId={1} questionType="input-type" />
+    </div>
   </div>
-);
-
-export default Tests;
+}
